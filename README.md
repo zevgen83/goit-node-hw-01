4f4719ea1c0cd5a31c8bac8ec86b7c0fae37a599
Zorkin Yevhen <108744200+zevgen83@users.noreply.github.com>
2023-04-16 08:51:51 +0300

    hw-01

---
+++ README.md
@@ -0,0 +1,8 @@
+# node-project
+
+Ссылки на фото скриншотов выполнения команд:
+
+1. list: https://monosnap.com/file/fT6zwxD8cAm8CHGGiFenMYjar8khhN
+2. get: https://monosnap.com/file/PMUEuW7eYusWKeL35XOqS7V2E2Mq2B
+3. add: https://monosnap.com/file/6kf0usjKGD3OkwwgUY8ZMM2bv0o2rC
+4. remove: https://monosnap.com/file/GJdEmpy64zE0khSt7i0aXZkZ07aMMB
---
+++ index.js
@@ -0,0 +1,36 @@
+const argv = require("yargs").argv;
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const list = await listContacts();
+      console.table(list);
+      break;
+
+    case "get":
+      const contactById = await getContactById(id);
+      console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const remove = await removeContact(id);
+      console.log(remove);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
